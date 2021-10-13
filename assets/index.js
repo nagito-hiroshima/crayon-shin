d09db90cac4f78f37cc9efd8a7b51b5815f574ee
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-10-13 14:48:11 +0900

    改行しない場合の処理追加

--- assets/index.js
+++ assets/index.js
@@ -5,14 +5,19 @@ function kureyon(){
         let Vlue = document.getElementById("text");    
         let inputValue = Vlue.value;
         let words = inputValue.split('　');
-        Area.innerHTML = words[0];
+        
             
             if(words[1]!=undefined){
                 words[1] = words[1].replace("ぞ","ゾ");
+                Area.innerHTML = words[0];
                 Area2.innerHTML = words[1];
+                Area3.innerHTML = "";
 
             }else{
+                words[0] = words[0].replace("ぞ","ゾ");
+                Area3.innerHTML = words[0];
                 Area2.innerHTML = "";
+                Area.innerHTML = "";
             }
         }
 
@@ -25,7 +30,8 @@ function se() {
         // 発言を作成
         const uttr = new SpeechSynthesisUtterance(text.value)
         // 速度 0.1-10 初期値:1 (倍速なら2, 半分の倍速なら0.5)
-        if (text.value.length>=15){
+        
+            if (text.value.length>=15){
             $('.Area').css('font-size','70px');
             console.log("aaaa")
             uttr.rate = 1.3
@@ -36,10 +42,12 @@ function se() {
         
         // 発言を再生 (発言キューに発言を追加)
         $('.Area').fadeIn();
+        $('.Areas').fadeIn();
         setTimeout( function() {
             speechSynthesis.speak(uttr)
             setTimeout( function(){
                 $('.Area').fadeOut();
+                $('.Areas').fadeOut();
             },4500);
         }, 500 );
         
